a65d313f97b98032026ae23be254b19202bcb293
Justin <justin@homeceuconnection.com>
2018-03-14 14:53:25 -0500

    Updated decorator to allow the result of the function to generate an alert instead of configuring it before the function executes

--- src/lib/Decorator/alert.ts
+++ src/lib/Decorator/alert.ts
@@ -1,15 +1,13 @@
-export function alert(type: string, message: string, title?: string, options?: any) {
-    return function (target: any, propertyName: string, descriptor: any) {
-        let method = descriptor.value;
+export function alert(target: any, propertyName: string, descriptor: any) {
+    let method = descriptor.value;
 
 
-        descriptor.value = function (...args: any[]) {
-            let result = method.apply(this, arguments);
-            window['$alert'][type](message, title, options);
-            return result;
-        };
+    descriptor.value = function (...args: any[]) {
+        let result = method.apply(this, arguments);
+        window['$alert'][result.type](result.message, result.title || null, result.options || {});
+        return result;
+    };
 
-    }
 }
 
 window['$alert'] = window['$alert'] || {
